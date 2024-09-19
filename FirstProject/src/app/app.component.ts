6203bffbb8c0eac0bad835463dbbcd75375d5c3c
Shubham-Parad <shubhamparad19@gmail.com>
2024-09-19 16:59:36 +0530

    first commit

---
+++ FirstProject/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FirstComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'Masstech Business Solutions.';
+}
