41c057e07f7aae7d97bd679984dc2e7e893c670f
Shubham-Parad <shubhamparad19@gmail.com>
2024-10-04 17:03:09 +0530

    "Updated form1 component with Angular forms, added validation and submission functionality; removed json pipe from home component"

--- RoutingProject/src/app/form1/form1.component.ts
+++ RoutingProject/src/app/form1/form1.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form1',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './form1.component.html',
   styleUrl: './form1.component.scss'
 })
 export class Form1Component {
-
+  checkFun(val:any){
+    console.log(val);
+  }
 }
