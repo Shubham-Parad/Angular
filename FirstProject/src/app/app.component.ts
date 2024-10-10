205974b186443f1f22a19b3e9e3e8582cf428c55
Shubham-Parad <shubhamparad19@gmail.com>
2024-10-10 16:19:43 +0530

    "Added GettersetterComponent to app component, updated app component HTML and TS files"

--- FirstProject/src/app/app.component.ts
+++ FirstProject/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SecondComponent } from './second/second.component';
 import { ThirdComponent } from './third/third.component';
 import { ChildComponent } from './child/child.component';
+import { GettersetterComponent } from './gettersetter/gettersetter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecondComponent, ThirdComponent, ChildComponent],
+  imports: [RouterOutlet, SecondComponent, ThirdComponent, ChildComponent, GettersetterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -15,4 +16,5 @@ export class AppComponent {
   title = 'Masstech Business Solutions.';
   name = 'John';
   getData: any;
+  public checkfn:boolean=true;
 }
---
+++ FirstProject/src/app/gettersetter/gettersetter.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gettersetter',
+  standalone: true,
+  imports: [],
+  templateUrl: './gettersetter.component.html',
+  styleUrl: './gettersetter.component.scss',
+})
+export class GettersetterComponent {
+  private _mydata: boolean = true;
+  public message: string = '';
+  get mydata(): boolean {
+    return this._mydata;
+  }
+
+  @Input()
+  set mydata(val: boolean) {
+    this._mydata = val;
+    if (val == true) {
+      this.message = 'You logged in';
+    } else {
+      this.message = 'You logged out';
+    }
+  }
+}
